bbfdf8307e6bfb4e6aa66a271d94ce7ba158cff5
Halil <halilpasho1@gmail.com>
2020-12-13 23:27:37 +0100

    added first commit with packages

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { persistor, store } from "./store/configureStore";
+import MainDsh from "./pages/MainDash";
+import { createBrowserHistory } from "history";
+import { Provider } from "react-redux";
+import Login from "./components/Login";
+
+const history = createBrowserHistory();
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="App">
+          <BrowserRouter basename="/" history={history}>
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/dashboard" component={MainDsh} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default App;
